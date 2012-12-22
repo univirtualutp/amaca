3b7feb6d7e8d1a577c6f5f02c2cf92b6a00f4937
Sebastian <uvdiseno@gmail.com>
2012-12-22 18:03:19 -0500

    front

--- theme/arialist/layout/frontpage.php
+++ theme/arialist/layout/frontpage.php
@@ -63,6 +63,7 @@ echo $OUTPUT->doctype() ?>
                         echo $OUTPUT->lang_menu();
                         echo $PAGE->headingmenu;
                     ?>
+                    <img src="<?php echo $OUTPUT->pix_url('uceva-logo', 'theme')?>" alt="UCEVA Unidad Cenral del Valle del Cauca" />
                 </div>
             </div>
         </div>
