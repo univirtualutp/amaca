733ec7427ec521511878ecace81e41dce7667ed7
Sebastian <uvdiseno@gmail.com>
2012-12-22 18:55:23 -0500

    logo

--- theme/arialist/layout/frontpage.php
+++ theme/arialist/layout/frontpage.php
@@ -63,9 +63,8 @@ echo $OUTPUT->doctype() ?>
                         echo $OUTPUT->lang_menu();
                         echo $PAGE->headingmenu;
                     ?>
-                    <div id="logo-verde">
+
                     <img src="<?php echo $OUTPUT->pix_url('uceva-logo', 'theme')?>" alt="UCEVA Unidad Cenral del Valle del Cauca" />
-                    </div>
                 </div>
             </div>
         </div>
